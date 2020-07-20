039a0830a408e870ca5dfd41c5c5b8577f3ff90c
TCollier0830 <55932149+TCollier0830@users.noreply.github.com>
2020-07-20 12:37:01 -0400

    Splitting functionality into separate files

--- MBMBMBOT/MBMBAMBot.py
+++ MBMBMBOT/MBMBAMBot.py
@@ -1,12 +1,4 @@
-import os
-import httplib2
-from bs4 import BeautifulSoup, SoupStrainer
-import requests
-import re
-from urllib.request import urlretrieve
-from pathlib import Path
-import PyPDF2
-from tika import parser
+import DataProcessing
 from tensorflow.keras.callbacks import LambdaCallback
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -16,13 +8,6 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.regularizers import l2
 import tensorflow as tf
-import numpy as np
-import random
-import sys
-import io
-import slate3k as slate
-import time
-import warnings
 
 warnings.simplefilter("ignore")
 tf.get_logger().setLevel('ERROR')
@@ -33,111 +18,6 @@ try:
 except:
   # Invalid device or cannot modify virtual devices once initialized.
   pass
-
-Curr = {}
-ll=0
-class Scrawl:
-
-    def __init__(self, TopLevel, desFolder, NumPages, keyPhrase):
-        self.TopLevel = TopLevel
-        self.desFolder = desFolder
-        self.NumPages = NumPages
-        self.keyPhrase = keyPhrase
-        SafeMake(self.desFolder)
-
-    def Scrawl(self):
-        for p in range(self.NumPages):
-        
-            http = httplib2.Http()
-
-            if p == 0:
-                status, response = http.request(self.TopLevel)
-            else:
-                status, response = http.request(self.TopLevel + '?_paged=' + str(p))
-            
-            for ii,link in enumerate(BeautifulSoup(response, 'html.parser',
-                                         parseOnlyThese=SoupStrainer('a'))):
-                if link.has_attr('href'):
-                    if self.keyPhrase in link['href'] and not link['href'] in Curr:
-                        Curr.update({link['href']: 1})
-                        print(link['href'])
-                        name = link['href'].split('/')[-2].replace('transcript-mbmbam-','').replace("-","_")
-                        r = requests.get(link['href'])
-                        soup = BeautifulSoup(r.text, "html.parser")
-                        for y in [list(map(int, re.findall(r'\d+', x))) for x in os.listdir(self.desFolder)]:
-                            if list(map(int, re.findall(r'\d+', name)))[0] in y:
-                                return
-                        desFile = os.path.join(self.desFolder, str(name) + '.pdf')
-                        f = Path(desFile)
-                        for i in soup.find_all('a', href=True):
-                            if ".pdf" in i.attrs['href']:
-                                ri = requests.get(i['href'])
-                                f.write_bytes(ri.content)
-
-        return
-
-def SafeMake(folder):
-    try:
-        return os.makedirs(folder)
-    except FileExistsError:
-        return Path(folder)
-
-
-def TextIt():
-    Brothers = os.path.join(os.getcwd(), "Brothers")
-    TextFiles = os.path.join(os.getcwd(), "TextFiles")
-    SafeMake(TextFiles)
-    for file in os.listdir(Brothers):
-        if os.path.join(TextFiles, file.replace("pdf","txt")) in os.listdir(TextFiles):
-            pass
-        oFile = open(os.path.join(TextFiles, file.replace("pdf","txt")), "w+", encoding="utf-8")
-        #raw = parser.from_file(os.path.join(Brothers,file))
-        #raw['content'].replace('\n','')
-        pdfFileObj = open(os.path.join(Brothers,file),"rb")
-        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-        for page in range(pdfReader.numPages):
-            pageObj = pdfReader.getPage(page)
-            body = pageObj.extractText()
-            SeparatedBody = [s.replace("\n","") + "\n" for s in body.split(".")]
-            for line in SeparatedBody:
-                if len(line) > 5:
-                    if "..." in line:
-                        line = line.replace("...","BIGBONGFGH")
-                    oFile.write(line.replace(".",".\n").replace("BIGBONGFGH", "..."))
-        oFile.close()
-    return
-
-def TextIt2():
-    Brothers = os.path.join(os.getcwd(), "Brothers")
-    TextFiles = os.path.join(os.getcwd(), "TextFiles")
-    os.SafeMake(TextFiles)
-    for file in os.listdir(Brothers):
-        oFile = open(os.path.join(TextFiles, file.replace("pdf","txt")), "w+", encoding="utf-8")
-        iFile = open(os.path.join(Brothers, file), "rb")
-        doc = slate.PDF(iFile)
-        for page in doc:
-            page = re.sub(r'\n+ ', '\n', page)
-            page = re.sub(r'\n+', '\n', page)
-            page = re.sub(r'[^\x00-\x7F]+','', page)
-            #page = re.sub(r'[^0-9a-zA-Z]+','', page)
-            if len(page)> 6: oFile.write(page[:-1])
-        iFile.close()
-        oFile.close()
-    return
-
-def CombineIt():
-    TextFiles = SafeMake(os.path.join(os.getcwd(), "TextFiles"))
-    oFile = open(os.path.join(TextFiles,"Beeg.txt"), "w+")
-    for file in os.listdir(TextFiles)[0:10]:
-        if not file == oFile:
-            iFile = open(os.path.join(TextFiles,file),"r+")
-            lines = iFile.readlines()
-            for line in lines:
-                oFile.write(line)
-                #oFile.write(line.replace("\n","").replace(".","").replace(",","").replace("!","").replace(":",""))
-            iFile.close()
-    oFile.close()
-    return
 
 def RNN():
     TextFiles = os.path.join(os.getcwd(), "TextFiles")
